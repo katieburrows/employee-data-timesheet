a9b55d9db226b672f6b56af72f3339e0c05de8f4
Samuel Mast <smast@Samuels-MacBook-Pro.local>
2018-02-01 12:22:54 -0700

    added js pseudo code

---
+++ js.js
@@ -0,0 +1,15 @@
+//firebase
+
+
+//global variables
+
+
+// initial
+	//write variable to page(function)
+
+//on click of submit button
+	//set input field to variables
+	//save variables to firebase
+	//write variables to page (function that we can reuse, delete all and reqrite values from firebase)
+		//clear the table
+		//calculations for 2 fields
